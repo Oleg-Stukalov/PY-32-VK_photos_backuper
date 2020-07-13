3b6ba6fc76eed91418da5946d3e309913603cd71
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-07-13 08:10:10 +0300

    Yandex token removed

--- venv/PY-32-VK-photos-backuper.py
+++ venv/PY-32-VK-photos-backuper.py
@@ -8,7 +8,7 @@ OAUTH_VK_URL = 'https://oauth.vk.com/authorize'
 #APP_ID = 7533990  #получен СОИ по ссылке https://vk.com/editapp?act=create
 TOKEN_VK = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008' #получен в Нетологии
 ######!!!!!!!!!!!!!!!!!!!!!!!
-TOKEN_YD = 'AgAAAAAy_UpkAADLW4DINWqLNUrUml_SsYrvX7U'
+TOKEN_YD = 'dsfds'
 #TOKEN_YD = input('Пожалуйста, введите токен учетной записи Яндекс, куда будем сохранять копию фото: ')
 print('Сохранен токен Яндекс: ', TOKEN_YD)
 
